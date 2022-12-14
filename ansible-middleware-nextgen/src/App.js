60feff56f1b2be6d14c16c132971018d0d8fc070
jrswanson4 <jrswanson4@wisc.edu>
2022-12-14 13:12:59 -0800

    Button Spacing corrected, working on background

--- ansible-middleware-nextgen/src/App.js
+++ ansible-middleware-nextgen/src/App.js
@@ -69,21 +69,21 @@ class SeparateTabs extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Masthead id="basic-example">
-          <MastheadContent>
+        <Split hasGutter style={{color: "#000000"}}>   
+          <SplitItem isFilled hasGutter> 
             <Tabs
               activeKey={this.state.activeTabKey}
               onSelect={this.handleTabClick}
               aria-label="Tabs in the seperate content example"
               role="region"
-              
+
             >
               <Tab
                 eventKey={0}
                 title={<TabTitleText>Home</TabTitleText>}
                 tabContentId="refTab1Section"
                 tabContentRef={this.contentRef1}
-                
+
               >
               </Tab>
               <Tab
@@ -111,16 +111,12 @@ class SeparateTabs extends React.Component {
                 tabContentRef={this.contentRef5}
               />
             </Tabs>
-            
-            
-            <Button variant="plain" component="a" href="https://github.com/" target="_blank">Github Link</Button> 
-            
-            
+          </SplitItem>
+          <SplitItem>
+            <Button variant="plain" component="a" href="https://github.com/" target="_blank">Github Link</Button>
             <Button variant="plain" component="a" href="https://galaxy.ansible.com/" target="_blank">Ansible Galaxy</Button>
-            
-            
-          </MastheadContent>
-        </Masthead>
+          </SplitItem>
+        </Split>
         <div>
           <TabContent
             /* Home */
