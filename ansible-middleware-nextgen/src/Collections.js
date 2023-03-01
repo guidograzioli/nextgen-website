6200ae0c40af8d02e57c2f2eb72ff66f67e690e4
Calvin Stafford <castaffo@redhat.com>
2023-03-01 13:04:38 -0600

    match card styling on collections page

--- ansible-middleware-nextgen/src/Collections.js
+++ ansible-middleware-nextgen/src/Collections.js
@@ -18,51 +18,51 @@ export default function Collections() {
         <Flex className="Gallery-options" direction={{default: "column"}} align={{ default: "alignLeft"}} flexWrap={{default: "wrap"}} spaceItems={{default: "spaceItems2xl"}}>  
         
             <Flex className="Gallery-options" spaceItems={{default: "spaceItems2xl"}}>
-            <Card isRounded isLarge> 
+            <Card isRounded isLarge className="Collections-card"> 
                 <CardTitle> Tab 1 Card </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
                 <CardFooter>Tab 1 Card footer</CardFooter>
             </Card>
             
-            <Card isRounded isLarge>
+            <Card isRounded isLarge className="Collections-card">
                 <CardTitle> Tab 1 Card </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
                 <CardFooter>Tab 1 Card footer</CardFooter>
             </Card>
             
-            <Card isRounded isLarge>
+            <Card isRounded isLarge className="Collections-card">
                 <CardTitle> Tab 1 Card </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
                 <CardFooter>Tab 1 Card footer</CardFooter>
             </Card>
             
-            <Card isRounded isLarge>
+            <Card isRounded isLarge className="Collections-card">
                 <CardTitle> Tab 1 Card </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
                 <CardFooter>Tab 1 Card footer</CardFooter>
             </Card>
             </Flex>
             <Flex className="Gallery-options-two" spaceItems={{default: "spaceItems2xl"}}>
-            <Card isRounded isLarge>
+            <Card isRounded isLarge className="Collections-card">
                 <CardTitle> Tab 1 Card </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
                 <CardFooter>Tab 1 Card footer</CardFooter>
             </Card>
             
-            <Card isRounded isLarge>
+            <Card isRounded isLarge className="Collections-card">
                 <CardTitle> Tab 1 Card </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
                 <CardFooter>Tab 1 Card footer</CardFooter>
             </Card>
             
-            <Card isRounded isLarge>
+            <Card isRounded isLarge className="Collections-card">
                 <CardTitle> Tab 1 Card </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
                 <CardFooter>Tab 1 Card footer</CardFooter>
             </Card>
             
             
-            <Card isRounded isLarge>
+            <Card isRounded isLarge className="Collections-card">
                 <CardTitle> Tab 1 Card </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
                 <CardFooter> Tab 1 Card footer</CardFooter> 
