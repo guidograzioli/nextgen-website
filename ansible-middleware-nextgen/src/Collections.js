e65fb0e96e4d30a2b53e52cf06f9651257214b5d
Calvin Stafford <castaffo@redhat.com>
2023-03-06 12:46:29 -0600

    add titles to collections cards
    collections card styling
    
    github links

--- ansible-middleware-nextgen/src/Collections.js
+++ ansible-middleware-nextgen/src/Collections.js
@@ -1,4 +1,5 @@
 import {
+    Button,
     Card,
     CardBody,
     CardFooter,
@@ -11,69 +12,69 @@ import {
     FlexItem
 } from '@patternfly/react-core';
 
+import GithubIcon from '@patternfly/react-icons/dist/esm/icons/github-icon';
+
 import AnisbleIcon from '@patternfly/react-icons/dist/esm/icons/ansible-tower-icon';
 
 export default function Collections() {
     return (
         <Flex className="Gallery-options" direction={{default: "column"}} align={{ default: "alignLeft"}} flexWrap={{default: "wrap"}} spaceItems={{default: "spaceItems2xl"}}>  
         
-            <Flex className="Gallery-options" spaceItems={{default: "spaceItems2xl"}}>
+            <Flex className="Gallery-options-two" spaceItems={{default: "spaceItems2xl"}}>
             <Card isRounded isLarge className="Collections-card"> 
-                <CardTitle> Tab 1 Card </CardTitle>
+                <CardTitle> Infinispan / Red Hat Data Grid </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
-                <CardFooter>Tab 1 Card footer</CardFooter>
+                <CardFooter><Button className="Button-color" variant="link" icon={<GithubIcon size="lg" className="Icon-color"/>} component="a" href="https://github.com/ansible-middleware/infinispan" target="_blank" hasGutter></Button></CardFooter>
             </Card>
             
             <Card isRounded isLarge className="Collections-card">
-                <CardTitle> Tab 1 Card </CardTitle>
+                <CardTitle> Keycloak / Red Hat Single Sign-On </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
-                <CardFooter>Tab 1 Card footer</CardFooter>
-            </Card>
-            
-            <Card isRounded isLarge className="Collections-card">
-                <CardTitle> Tab 1 Card </CardTitle>
-                <CardBody> Tab 1 Card Body</CardBody>
-                <CardFooter>Tab 1 Card footer</CardFooter>
-            </Card>
-            
-            <Card isRounded isLarge className="Collections-card">
-                <CardTitle> Tab 1 Card </CardTitle>
-                <CardBody> Tab 1 Card Body</CardBody>
-                <CardFooter>Tab 1 Card footer</CardFooter>
+                <CardFooter><Button className="Button-color" variant="link" icon={<GithubIcon size="lg" className="Icon-color"/>} component="a" href="https://github.com/ansible-middleware/keycloak" target="_blank" hasGutter></Button></CardFooter>
             </Card>
             </Flex>
             <Flex className="Gallery-options-two" spaceItems={{default: "spaceItems2xl"}}>
             <Card isRounded isLarge className="Collections-card">
-                <CardTitle> Tab 1 Card </CardTitle>
+                <CardTitle> Wildfly / Red Hat JBoss EAP </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
-                <CardFooter>Tab 1 Card footer</CardFooter>
+                <CardFooter><Button className="Button-color" variant="link" icon={<GithubIcon size="lg" className="Icon-color"/>} component="a" href="https://github.com/ansible-middleware/wildfly" target="_blank" hasGutter></Button></CardFooter>
             </Card>
             
             <Card isRounded isLarge className="Collections-card">
-                <CardTitle> Tab 1 Card </CardTitle>
+                <CardTitle> Tomcat / Red Hat JWS </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
-                <CardFooter>Tab 1 Card footer</CardFooter>
+                <CardFooter><Button className="Button-color" variant="link" icon={<GithubIcon size="lg" className="Icon-color"/>} component="a" href="https://github.com/ansible-middleware/jws" target="_blank" hasGutter></Button></CardFooter>
+            </Card>
+            </Flex>
+            <Flex className="Gallery-options-two" spaceItems={{default: "spaceItems2xl"}}>
+            <Card isRounded isLarge className="Collections-card">
+                <CardTitle> ActiveMQ / Red Hat AMQ Broker </CardTitle>
+                <CardBody> Tab 1 Card Body</CardBody>
+                <CardFooter><Button className="Button-color" variant="link" icon={<GithubIcon size="lg" className="Icon-color"/>} component="a" href="https://github.com/ansible-middleware/amq" target="_blank" hasGutter></Button></CardFooter>
             </Card>
             
             <Card isRounded isLarge className="Collections-card">
-                <CardTitle> Tab 1 Card </CardTitle>
+                <CardTitle> Kafka / Red Hat AMQ Streams </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
-                <CardFooter>Tab 1 Card footer</CardFooter>
+                <CardFooter><Button className="Button-color" variant="link" icon={<GithubIcon size="lg" className="Icon-color"/>} component="a" href="https://github.com/ansible-middleware/amq_streams" target="_blank" hasGutter></Button></CardFooter>
+            </Card>
+            </Flex>
+            <Flex className="Gallery-options-two" spaceItems={{default: "spaceItems2xl"}}>           
+            <Card isRounded isLarge className="Collections-card">
+                <CardTitle> Red Hat CSP Download </CardTitle>
+                <CardBody> Tab 1 Card Body</CardBody>
+                <CardFooter><Button className="Button-color" variant="link" icon={<GithubIcon size="lg" className="Icon-color"/>} component="a" href="https://github.com/ansible-middleware/redhat-csp-download" target="_blank" hasGutter></Button></CardFooter>
             </Card>
             
             
             <Card isRounded isLarge className="Collections-card">
-                <CardTitle> Tab 1 Card </CardTitle>
+                <CardTitle> JCliff </CardTitle>
                 <CardBody> Tab 1 Card Body</CardBody>
-                <CardFooter> Tab 1 Card footer</CardFooter> 
+                <CardFooter><Button className="Button-color" variant="link" icon={<GithubIcon size="lg" className="Icon-color"/>} component="a" href="https://github.com/ansible-middleware/ansible_collections_jcliff" target="_blank" hasGutter></Button></CardFooter> 
             </Card>
             </Flex>
             
-            </Flex>
-            
-            
-            
-        
+        </Flex>
         
     );
 }
